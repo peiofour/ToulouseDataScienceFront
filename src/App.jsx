98aa0032197269c5060b9216d01b020091e04efd
Pierre Fournier <pierre.fournier@ynov.com>
2018-11-30 15:31:24 +0100

    adding react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
-import Population from './components/population';
-
-
+import Population from './components/Population';
+import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from 'react-router-dom';
 
 class App extends Component {
+  home(){
+    return(
+      <div>
+        <p>Welcome home</p>
+      </div>
+    )
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <select>
-            <option value='foo'>Foo</option>
-            <option value='bar'>Bar</option>
-          </select>
-          <Population />
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/population">Population</Link>
+              </li>
+            </ul>
+            <Route exact path="/" component={ this.home } />
+            <Route path="/population" component={ Population } />
+          </header>
+        </div>
+      </Router>
     );
   }
 }
