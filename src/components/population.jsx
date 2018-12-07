3defa513ab312a50d6b4ff82e717c3a4d35957f4
Pierre Fournier <pierre.fournier@ynov.com>
2018-12-07 11:49:23 +0100

    react-chartjs to react-chartjs-2 chart with population by quartier now run

--- src/components/population.jsx
+++ src/components/population.jsx
@@ -1,34 +1,59 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Bar } from 'react-chartjs-2';
 
 export default class Population extends Component {
   constructor(props){
     super(props);
     this.state = {
-      test: []
+      pop: [],
+      quartiers: []
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
     axios.get('https://datascience-tls.scalingo.io/population')
-      .then(response => this.setState({test: response.data}))
+      .then(response => {
+        let popmoyen = response.data.map(foo => foo.MoyennePop)
+        let quartierfoo = response.data.map(bar => bar.Quartier)
+        this.setState({
+          pop : popmoyen,
+          quartiers : quartierfoo
+        })
+      })
   }
 
-  ListCities(){
-    const list = this.state.test.map((quartier)=> <li>{quartier.Quartier}</li>)
-    return list;
-  }
 
   render(){
+    const data = {
+      labels: this.state.quartiers,
+      datasets: [
+        {
+          label: 'Population moyenne par quartier',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: this.state.pop
+        }
+      ]
+    };
+
     return(
       <div className="button-container">
         <button className="button" onClick={this.handleClick}>
           Clique
         </button>
-        <ul>
-          {this.ListCities()}
-        </ul>
+          <Bar 
+            data={ data } 
+            width={1700}
+            height= {500}
+            options={{
+                  maintainAspectRatio: false
+            }}
+          />
       </div>
     )
   }
